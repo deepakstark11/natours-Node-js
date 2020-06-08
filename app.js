0280b0d1d07931db1c2379328d95da44773e42c3
Deepak Kumar <deepakramesh2000@gmail.com>
2020-06-08 11:49:45 +0530

    Added a comment

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
 const bookingRouter = require('./routes/bookingRoutes');
-
+//Start Express app
 const app = express();
 
 app.set('view engine', 'pug');
