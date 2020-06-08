f480421dcc6c85c4e0d110943e2309e04d525135
Deepak Kumar <deepakramesh2000@gmail.com>
2020-06-08 16:57:14 +0530

    Implemented Cors

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ const cookieParser = require('cookie-parser');
 
 const compression = require('compression');
 
+const cors = require('cors');
+
 const AppError = require('./utils/appError');
 
 const globalErrorHandler = require('./controllers/errorController');
@@ -40,6 +42,9 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 //Global Middlewares
+app.use(cors());
+
+app.options('*', cors());
 
 //Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
