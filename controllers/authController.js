669d27332f5a55b232b65897534d7a8598e14de3
Deepak Kumar <deepakramesh2000@gmail.com>
2020-06-08 16:16:13 +0530

    Added Heroku Configuration And Debugged

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,7 +18,7 @@ const signToken = id => {
   });
 };
 
-const createSendToken = (user, statusCode,req, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
 
   res.cookie('jwt', token, {
@@ -26,7 +26,7 @@ const createSendToken = (user, statusCode,req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+    secure: req.secure || req.headers('x-forwarded-proto') === 'https'
   });
   
 
